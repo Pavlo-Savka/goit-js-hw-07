9e50c3dd8397de4fb9b1aa98c484e86ef9c35532
Pavlo-Savka <savka.pavlo@gmail.com>
2023-02-13 00:58:16 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+for (let item of galleryItems) {
+    let largeImg = item.original; 
+    let smallImg = item.preview;
+    let description = item.description;
+gallery.insertAdjacentHTML('beforeend', `<div class= "gallery__item">
+  <a class="gallery__link" href="${largeImg}">
+    <img
+      class="gallery__image"
+      src="${smallImg}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div>`)
+};
+const selectImg = (event) => {
+    if (event.target.nodeName !== "IMG") {return};
+    const selectedImg = event.target.parentNode.href;
+}
+gallery.addEventListener("click", selectImg);
